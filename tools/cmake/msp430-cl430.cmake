6e2bab5aac4535caa9ab80f50475942f181a78ac
Justin J. Jordan <justinjordan1978@gmail.com>
2023-06-04 09:07:11 -0700

    WIP on build using ti tools

--- tools/cmake/msp430-cl430.cmake
+++ tools/cmake/msp430-cl430.cmake
@@ -1,23 +1,23 @@
 
-# MSP430_CCS_INSTALL should point to C:\ti\ccs1210\ccs, or something simillar depending on install location and version
+#CCS_INSTALL_ROOT should point to C:\ti\ccs1210, or something simillar depending on install location and version
 
-if (NOT DEFINED ENV{MSP430_CCS_INSTALL})
-	message(FATAL_ERROR "MSP430_CCS_INSTALL environment variable is not defined!")
+if (NOT DEFINED ENV{CCS_INSTALL_ROOT})
+	message(FATAL_ERROR "CCS_INSTALL_ROOT environment variable is not defined!")
 else()
-	message(STATUS "MSP430_CCS_INSTALL equals: $ENV{MSP430_CCS_INSTALL}")
+	message(STATUS "CCS_INSTALL_ROOT equals: $ENV{CCS_INSTALL_ROOT}")
 endif()
 
 # Set version being used here.  Multiple versions of the toolchain can be installed at same time
-set(MSP430_TOOLCHAIN_ROOT "$ENV{MSP430_CCS_INSTALL}/tools/compiler/ti-cgt-msp430_21.6.1.LTS")
+set(MSP430_TOOLCHAIN_ROOT "$ENV{CCS_INSTALL_ROOT}/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/bin")
 
 #cl430 is the cmd line tool that invokes either the asm, c/c++ compiler, or linker depending on flags
-find_program(CMAKE_ASM_COMPILER cl430 PATHS "${MSP430_TOOLCHAIN_ROOT}/bin" NO_DEFAULT_PATH)
-find_program(CMAKE_C_COMPILER cl430 PATHS "${MSP430_TOOLCHAIN_ROOT}/bin" NO_DEFAULT_PATH)
-find_program(CMAKE_CXX_COMPILER cl430 PATHS "${MSP430_TOOLCHAIN_ROOT}/bin" NO_DEFAULT_PATH)
-find_program(CMAKE_C_LINK_EXECUTABLE cl430 PATHS "${MSP430_TOOLCHAIN_ROOT}/bin" NO_DEFAULT_PATH)
+find_program(CMAKE_ASM_COMPILER cl430 PATHS "${MSP430_TOOLCHAIN_ROOT}" NO_DEFAULT_PATH)
+find_program(CMAKE_C_COMPILER cl430 PATHS "${MSP430_TOOLCHAIN_ROOT}" NO_DEFAULT_PATH)
+find_program(CMAKE_CXX_COMPILER cl430 PATHS "${MSP430_TOOLCHAIN_ROOT}" NO_DEFAULT_PATH)
+find_program(CMAKE_C_LINK_EXECUTABLE cl430 PATHS "${MSP430_TOOLCHAIN_ROOT}" NO_DEFAULT_PATH)
 
 #hex430 converts output file to hex file
-find_program(CG_TOOL_HEX hex430 PATHS "${MSP430_TOOLCHAIN_ROOT}/bin" NO_DEFAULT_PATH)
+find_program(CG_TOOL_HEX hex430 PATHS "${MSP430_TOOLCHAIN_ROOT}" NO_DEFAULT_PATH)
 
 
 set(CMAKE_SYSTEM_NAME Generic)
